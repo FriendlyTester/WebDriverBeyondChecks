cff153fd889287a6dc10fd4a08a0f5d3f1f3a850
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2016-11-04 15:43:30 +0000

    Bump to chromedriver and pom dependencies

--- webdriver/src/main/java/driverfactory/DriverFactory.java
+++ webdriver/src/main/java/driverfactory/DriverFactory.java
@@ -2,7 +2,6 @@ package driverfactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
