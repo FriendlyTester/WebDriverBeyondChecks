d34cc7154fff80fdbf6eb726ade41a6a4f250a9d
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2016-11-04 17:41:20 +0000

    Updates to visual checking to get it running on Phantomjs

--- visual/src/main/java/driverfactory/DriverFactory.java
+++ visual/src/main/java/driverfactory/DriverFactory.java
@@ -2,7 +2,9 @@ package driverfactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  * Created by richard on 17/07/2016.
@@ -12,8 +14,13 @@ public class DriverFactory
     public WebDriver create()
     {
         try{
-            if(System.getenv("browser").equals("firefox")){
-                return new FirefoxDriver();
+            if(System.getenv("browser").equals("phantomjs")){
+                String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36";
+
+                DesiredCapabilities caps = new DesiredCapabilities();
+                caps.setCapability(PhantomJSDriverService.PHANTOMJS_PAGE_SETTINGS_PREFIX + "userAgent", userAgent);
+
+                return new PhantomJSDriver(caps);
             } else {
                 ChromeDriver driver = generateChromeDriver();
                 return driver;
