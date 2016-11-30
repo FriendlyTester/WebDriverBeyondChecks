037c6d572e7015024c0f072477520b40ec837a69
Richard Bradshaw <richardbradshaw@gmail.com>
2016-11-30 10:19:18 +0000

    added windows system pproperty to driver factory

--- webdriver/src/main/java/driverfactory/DriverFactory.java
+++ webdriver/src/main/java/driverfactory/DriverFactory.java
@@ -33,8 +33,17 @@ public class DriverFactory
     }
 
     private ChromeDriver generateChromeDriver(){
-        String pathToChromeDriver = System.getProperty("user.dir") + "/src/main/java/driverfactory/chromedriver";
-        System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
+
+        //If you don't have ChromeDriver on your path, then uncomment the lines relevant to your OS
+
+        //OSX
+        //String pathToChromeDriver = System.getProperty("user.dir") + "/src/main/java/driverfactory/chromedriver";
+        //System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
+
+        //Windows
+        //String pathToChromeDriver = System.getProperty("user.dir") + "C:\\SomePathToSomewhere\\Path";
+        //System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
+
         return new ChromeDriver();
     }
 }
